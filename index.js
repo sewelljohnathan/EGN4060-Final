0a4a244606ecca7e0371c7c6890ad3e94cefbe21
sewell <sewelljohnathan@outlook.com>
2023-04-11 13:40:30 -0400

    Add A Star

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ function bfs() {
 
     let from = new Array(WORLD_H * WORLD_W);
 
-    while (queue.length > 0) {console.log(search);
+    while (queue.length > 0) {
 
         let index = queue.shift();
         if (visited.has(index)) {
@@ -74,6 +74,68 @@ function bfs() {
         cur = from[cur];
     }
 
+}
+
+function aStar() {
+    
+    // Initialize gList with the start node as 0
+    let gList = new Map();
+    gList.set(0, 0);
+
+    // Initialize the hList with the manhattan distance from the end
+    let hList = new Map();
+    for (let y = 0; y < WORLD_H; y++) {
+        for (let x = 0; x < WORLD_W; x++) {
+
+            let index = y * WORLD_W + x;
+            hList.set(index, Math.abs((WORLD_W - 1) - x) + Math.abs((WORLD_H - 1) - y));
+
+        }
+    }
+
+    // Initialize the queue and list of seen nodes
+    let queue = new Array();
+    queue.push(0);
+    let seen = new Set();
+    seen.add(0);
+    let from = new Array(WORLD_H * WORLD_W);
+    
+    while (queue.length > 0) {
+
+        // Sort the queue by f value
+        queue.sort((a, b) => {
+            let fA = gList.get(a) + hList.get(a);
+            let fB = gList.get(b) + hList.get(b);
+            return fA - fB;
+        });
+
+        // Take the lowest index
+        let index = queue.shift();
+        search.push(index);
+
+        if (index == mazeEnd) {
+            break;
+        }
+
+        // Add the neighbors to the queue
+        maze[index].forEach(neighbor => {
+            if (!seen.has(neighbor)) {
+
+                seen.add(neighbor);
+                gList.set(neighbor, gList.get(index)+1); // increment the neighbor g
+                queue.push(neighbor);
+                from[neighbor] = index;
+            }
+        })
+    }
+
+    // Generate the backwards path
+    let cur = mazeEnd;
+    while (cur != 0) {
+
+        path.push(cur);
+        cur = from[cur];
+    }
 }
 
 function runSolver(func) {
